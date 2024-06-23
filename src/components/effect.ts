74ff23280bf2ac0bc45a9d8727364d7b4db8b2c0
joska <153557237+joska-p@users.noreply.github.com>
2024-06-23 15:49:46 +0200

    simple things

--- src/components/effect.ts
+++ src/components/effect.ts
@@ -1,33 +1,77 @@
-import { Particle } from "./particle.js"
-import { throttle } from "./utils.js"
+import {Particle} from './particle.js'
+import {throttle, getParentSize} from './utils.js'
+import {Vector2} from './vector2.js'
+
+interface EffectProps {
+  id: string
+  debug?: boolean
+}
 
 class Effect {
   canvas: HTMLCanvasElement
   ctx: CanvasRenderingContext2D
   particles: Array<Particle>
+  debug: boolean
+  scale: Vector2
 
-  constructor(canvas: HTMLCanvasElement) {
-    this.canvas = canvas
-    this.ctx = canvas.getContext("2d") as CanvasRenderingContext2D
+  constructor({id, debug}: EffectProps) {
+    this.canvas = document.getElementById(id) as HTMLCanvasElement
+    this.ctx = this.canvas.getContext('2d') as CanvasRenderingContext2D
     this.particles = []
+
+    this.debug = debug ?? false
+    this.scale = new Vector2(50, 50)
+  }
+
+  #resizeCanvas() {
+    const {x, y} = getParentSize(this.canvas)
+
+    // the canvas need to be resize to the closest multiple of the scale that fits the parent
+    this.canvas.width = Math.ceil(x / this.scale.x) * this.scale.x
+    this.canvas.height = Math.ceil(y / this.scale.y) * this.scale.y
+
+    this.update()
   }
 
   #handlePointerClick(event: MouseEvent) {
     const pixelX = event.clientX - this.canvas.offsetLeft
     const pixelY = event.clientY - this.canvas.offsetTop
-    this.particles.push(new Particle({ position: { x: pixelX, y: pixelY } }))
+    this.particles.push(new Particle({position: new Vector2(pixelX, pixelY), color: '#ff0000'}))
     console.log(this.particles)
     this.update()
   }
 
   #initEventsListener() {
-    this.canvas.addEventListener("click", throttle(this.#handlePointerClick.bind(this), 100))
+    window.addEventListener('resize', throttle(this.#resizeCanvas.bind(this), 100))
+    this.canvas.addEventListener('click', throttle(this.#handlePointerClick.bind(this), 100))
   }
 
   draw() {
     for (const particle of this.particles) {
       particle.draw(this.ctx)
     }
+
+    if (this.debug) this.drawDebug()
+  }
+
+  drawDebug() {
+    this.ctx.save()
+
+    this.ctx.beginPath()
+    this.ctx.strokeStyle = '#ff0000'
+    this.ctx.globalAlpha = 0.5
+
+    for (let x = 0; x < this.canvas.width; x += this.scale.x) {
+      this.ctx.moveTo(x, 0)
+      this.ctx.lineTo(x, this.canvas.height)
+    }
+    for (let y = 0; y < this.canvas.height; y += this.scale.y) {
+      this.ctx.moveTo(0, y)
+      this.ctx.lineTo(this.canvas.width, y)
+    }
+    this.ctx.stroke()
+
+    this.ctx.restore()
   }
 
   update() {
@@ -35,8 +79,10 @@ class Effect {
   }
 
   init() {
+    this.#resizeCanvas()
     this.#initEventsListener()
+    this.update()
   }
 }
 
-export { Effect }
+export {Effect}
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,8 @@
-import "./style.css"
-import { Canvas } from "./components/canvas"
-import { Effect } from "./components/effect"
+import './style.css'
+import {Effect} from './components/effect'
 
-const canvasElement = document.getElementById("canvas")
+const effect = new Effect({id: 'canvas', debug: true})
 
-const effect = new Effect(canvasElement as HTMLCanvasElement)
+console.log(effect)
 
-const canvas = new Canvas({
-  canvas: canvasElement as HTMLCanvasElement,
-  scale: { x: 30, y: 30 },
-  color: "red",
-  debug: true,
-})
-
-canvas.init()
 effect.init()
